859ecebc80437b098afba4614cae9293f545fd50
imsidthakur <106941477+imsidthakur@users.noreply.github.com>
2024-10-01 07:00:49 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
+  public class Product{
+    public int product(int x,int y){}
+    public int product(int x,int y,int z){}
+    public double product(double x, double y){}
+  }
 
+  Product p = new Product;
+  p.product(1,1);
+  p.product(1,1,1);
+  p.product(11.02,1.33);
 }
